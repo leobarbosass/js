6cb2d1c3f6f46ff4f41cf74e5407a2d1e24b8dee
LEONARDO BARBOSA SANTOS <22102366@dsn127m1242373.cfp127aluno.local>
2022-08-08 11:15:34 -0300

    att

--- exercicios/app.js
+++ exercicios/app.js
@@ -7,9 +7,10 @@
     
 console.log('\n--- VERIFICAR SE O ALUNO(A) PASSOU NA FACULDADE --- \n')
 
-const { exercico, calcularMedia } = require('./modulos/exercico.js')
+const { exercico, calcularMedia, verificarAp } = require('./modulos/exercico.js')
 
 var readline = require('readline')
+const { exit } = require('process')
 
 var entradaDados = readline.createInterface({
 
@@ -20,49 +21,130 @@ var entradaDados = readline.createInterface({
 entradaDados.question('Digite o nome do aluno(a): \n', function (nomeAlun){
     let nome = nomeAlun
 
+    if( nomeAlun == ''){
+        console.log('O campo deve ser prenchido')
+        exit()
+    }
+
     entradaDados.question('Digite o nome do professor(a): \n', function(nomeProf){
         let prof = nomeProf
+
+        if( nomeProf == ''){
+            console.log('O campo deve ser prenchido')
+            exit()
+        }
 
         entradaDados.question('Digite o sexo do aluno(a): \n', function(sexoAlun){
             let sexoA = sexoAlun
 
+            if( sexoAlun == ''){
+                console.log('O campo deve ser prenchido')
+                exit()
+            }
+
             if(sexoA == 'feminino'){
-                sexoA = 'aluna'
+                sexoA = 'A aluna'
             }else{
-                sexoA = 'aluno'
+                sexoA = 'O aluno'
             }
 
             entradaDados.question('Digite o sexo do professor(a): \n', function(sexoProf){
                 let sexoP = sexoProf
 
-            if(sexoP == 'feminino'){
-                sexoP = 'professora'
-            }else{
-                sexoA = 'professor'
-            }
+                if( sexoProf == ''){
+                    console.log('O campo deve ser prenchido')
+                    exit()
+                }
+
+                if(sexoP == 'feminino'){
+                    sexoP = 'A professora'
+                }else{
+                    sexoP = 'O professor'
+                }
 
                 entradaDados.question('Digite o nome do curso: \n', function(curso){
                     let curs = curso
 
+                    if( curso == ''){
+                        console.log('O campo deve ser prenchido')
+                        exit()
+                    }
+                    
                     entradaDados.question('Digite o nome da disciplina: \n', function(disciplina){
                         let disc = disciplina
+
+                        if( disciplina == ''){
+                            console.log('O campo deve ser prenchido')
+                            exit()
+                        }
 
                         entradaDados.question('Entrar com valor da nota 1: \n', function(valor1){
                             let nota1 = valor1
                     
+                            if( valor1 == ''){
+                                console.log('O campo deve ser prenchido')
+                                exit()
+                            }
+
                         entradaDados.question('Entrar com o valor da nota 2: \n', function(valor2){
                             let nota2 = valor2
-                    
+                            
+                            if( valor2 == ''){
+                                console.log('O campo deve ser prenchido')
+                                exit()
+                            }
+
                         entradaDados.question('Entrar com o valor da nota 3: \n', function(valor3){
                             let nota3 = valor3
+
+                            if( valor3 == ''){
+                                console.log('O campo deve ser prenchido')
+                                exit()
+                            }
+
 
                         entradaDados.question('Entrar com o valor da nota 4: \n', function(valor4){
                             let nota4 = valor4
 
-                            console.log('Ola ' + sexoA + 'sua media eh ' + media)
+                            if( valor4 == ''){
+                                console.log('O campo deve ser prenchido')
+                                exit()
+                            }
+
+                        
+                            let media = calcularMedia(nota1, nota2, nota3, nota4)
+                          
+                            console.log(sexoA + ' ' + nome +' esta ' + verificarAp(media) + ' na disciplina de ' + disc)
+                            console.log('Curso : ' + curs)
+                            console.log(sexoP + ': ' + prof)
+                            console.log('Notas do aluno: ' + nota1 + ' ' + nota2 + ' ' + nota3 + ' ' + nota4)
+                            console.log('Media Final: ' + media)
 
 
-                            entradaDados.close()
+
+                            entradaDados.question('Caso ' + sexoA + ' ' + nome + ' tenha ficado de exame, qual a nota do mesmo, se ele nao ficou de recuperacao digite "nao"\n', function(notaExa){
+                                let notaExame = notaExa
+                                
+                                if( notaExame == 'nao'){
+                                    console.log('O usuario verificou suas notas')
+                                    exit()
+                                }else{
+                                    console.log(sexoA + ' ' + nome +' esta ' + verificarAp(media) + ' na disciplina de ' + disc)
+                                    console.log('Curso : ' + curs)
+                                    console.log(sexoP + ': ' + prof)
+                                    console.log('Notas do aluno: ' + nota1 + ' ' + nota2 + ' ' + nota3 + ' ' + nota4)
+                                    console.log('Notas do exame: ' + notaExame)
+                                    console.log('Media Final: ' + parseFloat(media) + parseFloat(notaExame))
+                                }
+
+                                if( valor4 == ''){
+                                    console.log('O campo deve ser prenchido')
+                                    exit()
+                                }
+
+                                entradaDados.close()
+                            })
+
 
                             
                         })
--- exercicios/modulos/exercico.js
+++ exercicios/modulos/exercico.js
@@ -5,8 +5,8 @@
     - Versão: 1.0
 */
 
-const calcularMedia = function(nota1, nota2, nota3, nota4){
-    let media 
+const calcularMedia = function(nota1, nota2, nota3, nota4){ 
+    let media
 
     if(nota1 == '' || nota2 == '' || nota3 == '' || nota4 == ''){
         console.log('Os campos de notas devem ser prenchidos')
@@ -14,19 +14,24 @@ const calcularMedia = function(nota1, nota2, nota3, nota4){
     }else{
         media = (parseFloat(nota1) + parseFloat(nota2) + parseFloat(nota3) + parseFloat(nota4)) /4
 
-        console.log('A media do aluno eh de: ' + media)
-        
-    }if (media > 70) {
-        console.log('O usuario foi aprovado!')
+        return media
+    }
+
+}
+
+const verificarAp =  function(media){
+    if (media >= 70) {
+        return 'aprovado'
 
     }else if (50 < media > 69) {
-        console.log('O usuario esta em exame')
+        return 'exame'
 
     }else{
-        console.log('O usuario foi reprovado')
+        return 'reprovado'
     }
 }
 
 module.exports = {
-    calcularMedia
+    calcularMedia,
+    verificarAp
 }
